149d38501da30864953dc276cc887a8ddfea8353
DawonEllaKim <dawonella0411@gmail.com>
2022-01-13 12:22:59 +0900

    Add - minor styling

--- src/routes/App/components/App.tsx
+++ src/routes/App/components/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import './App.scss';
@@ -19,15 +20,19 @@ function App() {
 	};
 
     return (
-        <div className={ themeChange !== "darkMode" ? "app" : "app dark-mode"}>
+        <Wrap className={ themeChange !== "darkMode" ? "app" : "app dark-mode"}>
+					{themeChange === "lightMode" ? <Hello style={{color: "#8A4D76"}} onClick={switchToDarkMode} > Dark mode </Hello> :
+					<Hello style={{color: "#fff"}} onClick={switchToLightMode}> Light mode </Hello>}
             <div className="level">
                 <div>
 										<h1 className="title" data-testid="header">Packative Dark Mode</h1>
                 </div>
 
-                <button className="app__dark-mode-btn icon level-right">
+                <ThemeButton>
+								<button className="app__dark-mode-btn icon level-right">
 									{ themeChange !== "darkMode" ? <FontAwesomeIcon icon={faMoon}  onClick={switchToDarkMode} /> : <FontAwesomeIcon icon={faSun} style={{ color: "#F89709" }}  onClick={switchToLightMode} /> }
                 </button>
+								</ThemeButton>
             </div>
 
             <div className="columns">
@@ -51,14 +56,83 @@ function App() {
                 </div>
             </div>
 
-            <section className="section">
-                <div className="buttons level-right">
-                    <a className="button is-primary">Save</a>
-                    <a className="button is-link">Submit</a>
-                </div>
-            </section>
-        </div>
+            
+                <ButtonsWrap>
+                    <Btn className="button is-primary">Save</Btn>
+                    <Btn className="button is-link">Submit</Btn>
+                </ButtonsWrap>
+            
+        </Wrap>
     );
 }
+
+const Wrap = styled.div`
+	position: relative;
+`
+
+const Hello = styled.button`
+		background-color: transparent;
+		border: none;
+		font-size: 14px;
+		font-weight: bold;
+		position: absolute;
+		top: 10px;
+		right: 5px;
+		cursor: pointer;
+
+		@media (min-width: 441px) {
+			display: none;
+		}
+`
+
+const ThemeButton = styled.div`
+	@media (max-width: 780px) {
+		box-sizing: border-box;
+		position: fixed;
+		right: 0;
+		top: 70px;
+		z-index: 1;
+		padding: 5px 8px;
+		border-top-left-radius: 5px;
+		border-bottom-left-radius: 5px;
+		background-color: white;
+	}
+
+	@media (max-width: 440px) {
+		display: none;
+	}
+`
+
+const ButtonsWrap = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: right;
+	box-sizing: border-box;
+	width: 100%;
+	padding: 0 24px;
+	margin-top: 10px;
+
+	@media (max-width: 767px) {
+		justify-content: center;
+	}
+
+	@media (max-width: 440px) {
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 0;
+	}
+`
+
+const Btn = styled.button`
+	width: 136px;
+	height: 46px;
+	margin-left: 8px;
+
+	@media (max-width: 440px) {
+		width: 100%;
+		margin-bottom: 10px;
+	}
+`
 
 export default App;
--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,5 +1,6 @@
 // import { Route } from 'react-router';
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import styled from 'styled-components';
 import App from './App';
 import AnotherPage from './App/components/AnotherPage';
 
@@ -8,12 +9,14 @@ const Main = (): JSX.Element => {
 	return (
 		<>
 			<Router>
-				<Link to="/">
-					<button>Main Page</button>
-				</Link>
-				<Link to="/anotherPage">
-					<button>Another Page</button>
-				</Link>
+				<Nav>
+					<Link to="/">
+						<button>Main Page</button>
+					</Link>
+					<Link to="/anotherPage">
+						<button>Another Page</button>
+					</Link>
+				</Nav>
 				<Routes>
 					<Route path="/" element={<App/>}/>
 					<Route path="/anotherPage" element={<AnotherPage/>}/>
@@ -22,5 +25,27 @@ const Main = (): JSX.Element => {
 		</>
 	);
 };
+
+const Nav = styled.div`
+	width: 80%;
+	margin: 10px auto;
+	text-align: right;
+	@media (max-width: 520px) {
+		text-align: center;
+	}
+	button{
+		box-sizing: border-box;
+		margin-left: 10px;
+		border-radius: 5px;
+		:hover {
+			box-sizing: border-box;
+			border: 1px solid #FA7C91;
+			color: #FA7C91;
+			font-weight: bold;
+			cursor: pointer;
+			transition: 0.2s ease-in-out;
+		}
+	}
+`
 
 export default Main;
